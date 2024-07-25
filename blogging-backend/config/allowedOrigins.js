c57b1fed2c7a65bfec93f4e76e20a772919be930
Ayush Luthra <ayushluthra62@gmail.com>
2024-07-25 14:08:46 +0530

    changes

--- blogging-backend/config/allowedOrigins.js
+++ blogging-backend/config/allowedOrigins.js
@@ -4,7 +4,6 @@ const allowedOrigins = [
     'http://localhost:4200',
     'https://mellifluous-khapse-323cac.netlify.app',
     'https://blogging-website-backend-1-jjlx.onrender.com'
-
 ];
 
 module.exports = allowedOrigins;
