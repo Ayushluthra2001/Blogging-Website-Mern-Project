4746292b578545527d6b50b1b668b223cce91223
Ayush Luthra <ayushluthra62@gmail.com>
2024-07-25 11:54:04 +0530

    Normalize all line endings

---
+++ blogging-backend/config/allowedOrigins.js
@@ -0,0 +1,10 @@
+
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:4200',
+    'https://loquacious-seahorse-614b70.netlify.app/',
+    'https://blogging-website-backend-1-jjlx.onrender.com'
+
+];
+
+module.exports = allowedOrigins;
---
+++ blogging-frontend/src/constants.js
@@ -0,0 +1,4 @@
+
+export const BASE_URL = "https://blogging-website-backend-1-jjlx.onrender.com";
+export const LOCAL_URL = "http://localhost:3001/";
+export const IS_AUTHENTICATED = false;
